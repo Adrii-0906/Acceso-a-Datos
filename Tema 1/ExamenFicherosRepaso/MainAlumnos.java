a2a1200992e4ceb9ade493f4d52c814174fc4c1b
Adrii-0906 <adrianranaescote4@gmail.com>
2025-10-20 12:22:14 +0200

    Examen terminado

--- Tema 1/ExamenFicherosRepaso/MainAlumnos.java
+++ Tema 1/ExamenFicherosRepaso/MainAlumnos.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainAlumnos {
@@ -16,82 +15,176 @@ public class MainAlumnos {
             System.out.println("4 - Calcular la media de notas");
             System.out.println("0 - Salir del programa \n");
 
-            System.out.println("Dime que accion quieres realizar(0 = fin del programa): ");
+            System.out.print("Dime que accion quieres realizar(0 = fin del programa): ");
             option = entrada.nextInt();
 
-            switch (option) {
-                case 1:
-                    anadirAlumnos();
-                case 2:
-                    delvolverId();
-                case 3:
-                    insertarNotas();
-                case 4:
-                    calcularMedia();
-                case 0:
-                    System.out.println("Saliendo del programa");
-                    break;
-
-                default:
-                    System.out.println("Opcion invalida, intentalo de nuevo");
-            }
+           if (option == 1) {
+               anadirAlumnos();
+           } else if (option == 2) {
+               delvolverId();
+           } else if (option == 3) {
+               insertarNotas();
+           } else if (option == 4) {
+               calcularMedia();
+           }
         } while (option != 0);
     }
 
 
-    static ArrayList<Alumnos> alumnosList = new ArrayList<>();
     public static void anadirAlumnos() {
-        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        do {
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Dime el nombre del alumno: ");
-        String nombre = sc.nextLine();
+            System.out.println("Dime el nombre del alumno: ");
+            String nombre = sc.nextLine();
 
-        System.out.println("Dime los apellidos del alumno: ");
-        String apellido = sc.nextLine();
+            System.out.println("Dime los apellidos del alumno: ");
+            String apellido = sc.nextLine();
 
-        System.out.println("Dime la fecha de nacimiento del alumno: ");
-        String fechaNac = sc.nextLine();
+            System.out.println("Dime la fecha de nacimiento del alumno: ");
+            String fechaNac = sc.nextLine();
 
-        System.out.println("Dime la clase del alumno: ");
-        String clase = sc.nextLine();
+            System.out.println("Dime la clase del alumno: ");
+            String clase = sc.nextLine();
 
-        try {
-            File file = new File("Alumnos.txt");
-            BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+            try {
+                File file = new File("Alumnos.txt");
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
 
-            Alumnos cadena = new Alumnos(nombre,apellido,fechaNac,clase);
+                int id = 0;
 
-            alumnosList.add(cadena);
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    id++;
+                }
 
-            bw.write(cadena.toString());
-            bw.newLine();
+                String cadena = id + "|" + nombre + "|" + apellido + "|" + fechaNac + "|" + clase;
 
-            bw.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+                bw.write(cadena);
+                bw.newLine();
+
+                bw.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Si quieres anadir otro alumno pulsa '1' si no pulsa '0'");
+            opcion = Integer.parseInt(sc.nextLine());
+        } while (opcion!= 0);
     }
 
     public static void delvolverId() {
-//        Scanner entrada = new Scanner(System.in);
-//
-//        System.out.println("Dime el nombre del alumno: ");
-//        String nombre = entrada.nextLine();
-//
-//        System.out.println("Dime el apellido del alumno: ");
-//        String apellido = entrada.nextLine();
-//
-//        for (Alumnos a : alumnosList) {
-//            a.obtenerId(nombre, apellido);
-//        }
-//
-      }
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Dime el nombre del alumno: ");
+        String nombre = entrada.nextLine();
+
+        System.out.println("Dime el apellido del alumno: ");
+        String apellido = entrada.nextLine();
+
+        sacarId(nombre, apellido);
+
+        System.out.println("El id de " + nombre + " " + apellido + " = " + sacarId(nombre, apellido));
+
+    }
+
+
+    public static int sacarId(String nombre, String apellidos){
+        try {
+            File file = new File("Alumnos.txt");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+
+                String[] partes = linea.split("\\|");
+
+                if (nombre.equals(partes[1]) & apellidos.equals(partes[2])) {
+                    return Integer.parseInt(partes[0]);
+                }
+            }
+
+            br.close();
+
+        } catch (Exception e) {
+            System.out.printf("El alumno con nombre" + nombre + "apellido" + apellidos + " no se encuentra en el archivo");
+        }
+        return 0;
+    }
 
     public static void insertarNotas() {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Dime el nombre del alumno: ");
+        String nombre = entrada.nextLine();
+
+        System.out.println("Dime el apellido del alumno: ");
+        String apellido = entrada.nextLine();
+
+
+        try {
+            File file = new File("Notas.txt");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+
+            System.out.print("Dime las notas de " + nombre + " separadas por ';' ");
+            String nota = entrada.nextLine();
+
+
+            String cadena = sacarId(nombre, apellido) + "|" + nota;
+
+            bw.write(cadena);
+            bw.newLine();
+
+            bw.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     public static void calcularMedia() {
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Dime el nombre del alumno: ");
+        String nombre = entrada.nextLine();
+
+        System.out.println("Dime el apellido del alumno: ");
+        String apellido = entrada.nextLine();
+
+        try {
+            File file = new File("Notas.txt");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String linea;
+            double suma = 0;
+
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split("\\|");
+
+                int idtenido = Integer.parseInt(partes[0]);
+
+               if (idtenido == sacarId(nombre, apellido)) {
+                   String[] partes1 = partes[1].split(";");
+                   System.out.println("La media del alumno es: ");
+
+                   for (int i = 0; i < partes1.length; i++) {
+                       suma += Double.parseDouble(partes1[i]);
+                   }
+                   double media = suma / partes1.length;
+
+                   System.out.println("Media = " + media);
+
+                   break;
+               }
+            }
+
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
